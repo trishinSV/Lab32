fd7f2f71d8f6471e3b37d732f5fee1df28737f25
trishinSV <Sergey@MacBook-Pro-Sergej.local>
2017-09-27 13:12:25 +0300

    Initial Commit

---
+++ Lab32/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab32
+//
+//  Created by Сергей Тришин on 27.09.17.
+//  Copyright © 2017 Сергей Тришин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
